c9e326844d43c38e96b1cd5004705c123946d777
saps-git <saptarshi0009@gmail.com>
2022-04-08 23:57:21 +0530

    08/04/2022

--- src/com/dsa/linkedlist/LL.java
+++ src/com/dsa/linkedlist/LL.java
@@ -31,7 +31,7 @@ public class LL {
         size += 1;
     }
     
-    public void insertLast(int data) {
+    public void insertLast(int data) { //insert at the end with tail pointer
         if(tail == null) {
             insertFirst(data);
             return;
@@ -40,11 +40,16 @@ public class LL {
         Node node = new Node(data);
         tail.next = node;
         tail = node;
+        size += 1;
     }
 
-    public void insert(int data) {
+    public void insert(int data) { //inseret at the end without tail
         Node node = new Node(data);
         node.next = null;
+        if(head == null) {
+            insertFirst(data);
+            return;
+        }
         Node temp = head;
         while(temp.next != null) {
             temp = temp.next;
@@ -53,8 +58,72 @@ public class LL {
         size += 1;
     }
 
+    public void insertAtPos(int data, int pos) {
+        if(pos == 0) {
+            insertFirst(data);
+            return;
+        }
+
+        Node node = new Node(data);
+        Node temp = head;
+        while(pos != 1) {
+            temp = temp.next;
+            pos--;
+        }
+
+        node.next = temp.next;
+        temp.next = node;
+        size += 1;
+    }
+
+    public int deleteFirst() {
+        int val = head.data;
+        head = head.next;
+        size -= 1;
+        return val;
+    }
+
+    public int deleteLast() {
+        if(size == 1) return deleteFirst();
+        if(size == 2) {
+            int ans = head.next.data;
+            head.next = null;
+            return ans;
+        }
+        Node temp = head;
+        while(temp.next.next != null) {
+            temp = temp.next;
+        }
+        int val = temp.next.data;
+        temp.next = null;
+        size -= 1;
+        return val;
+    }
+
+    public void display() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.print("END");
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        
+        LL linkedList = new LL();
+        linkedList.insert(2);
+        linkedList.insert(4);
+        linkedList.insert(5);
+        linkedList.insert(10);
+        linkedList.insert(13);
+
+        linkedList.insertAtPos(15,1);
+        linkedList.display();
+
+        System.out.println(linkedList.deleteFirst());
+        System.out.println(linkedList.deleteLast());
+
     }
 }
 
